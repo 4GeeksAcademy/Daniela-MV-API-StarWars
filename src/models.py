3961bfe85d7cd10b18944ce8f1022103a655f240
Daniela-MV <danmv.0991@gmail.com>
2025-06-05 04:00:41 +0000

    Seguir con planets

--- src/models.py
+++ src/models.py
@@ -6,6 +6,8 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    user_name: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
     password: Mapped[str] = mapped_column(nullable=False)
     is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
@@ -14,6 +16,44 @@ class User(db.Model):
     def serialize(self):
         return {
             "id": self.id,
+            "name": self.name,
+            "user_name": self.user_name,
             "email": self.email,
             # do not serialize the password, its a security breach
+        }
+
+class Character(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), nullable=False)
+    gender: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    hair_color: Mapped[str] = mapped_column(String(120), nullable=False)
+    eye_color: Mapped[str] = mapped_column(String(120), nullable=False)
+    birth_year: Mapped[str] = mapped_column(String(120), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "gender": self.gender,
+            "hair_color": self.hair_color,
+            "eye_color": self.eye_color,
+            "birth_year": self.birth_year,
+        }
+
+class Planets(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(120), nullable=False)
+    gender: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
+    hair_color: Mapped[str] = mapped_column(String(120), nullable=False)
+    eye_color: Mapped[str] = mapped_column(String(120), nullable=False)
+    birth_year: Mapped[str] = mapped_column(String(120), nullable=False)
+
+    def serialize(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "gender": self.gender,
+            "hair_color": self.hair_color,
+            "eye_color": self.eye_color,
+            "birth_year": self.birth_year,
         }
